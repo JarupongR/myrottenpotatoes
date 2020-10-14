c2fab756c72c52c38eafc0d9d2535d59d9504ebf
Jarupong <s6101012620083@email.kmutnb.ac.th>
2020-10-14 22:36:21 +0700

    Chapter5.5[Restful]:Finished on basic Review(controller,route,view)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 end
 
 Myrottenpotatoes::Application.routes.draw do
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
   root :to => redirect('/movies')
   get  'auth/:provider/callback' => 'sessions#create'
   get  'logout' => 'sessions#destroy'
