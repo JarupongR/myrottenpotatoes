d1606258dbe64eddcdcdca8494c4477db1d08a45
Jarupong <s6101012620083@email.kmutnb.ac.th>
2020-10-17 15:13:08 +0700

    [Refactoring]:Add a list of comment by each user in show.thml.haml

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -45,7 +45,7 @@ class ReviewsController < ApplicationController
 
     private
       def review_params
-        params.require(:review).permit(:potatoes)
+        params.require(:review).permit(:potatoes, :comments)
       end
 
   end
