cf8aaef0e0490fac7deacb1012a1ebeed312b1ed
Jarupong <s6101012620083@email.kmutnb.ac.th>
2020-10-18 14:32:27 +0700

    Fix current_user review error at migratep[6.0]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_170434) do
+ActiveRecord::Schema.define(version: 2020_10_14_141111) do
 
   create_table "moviegoers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_10_05_170434) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "potatoes"
+    t.text "comments"
+    t.integer "moviegoer_id"
+    t.integer "movie_id"
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["moviegoer_id"], name: "index_reviews_on_moviegoer_id"
   end
 
 end
